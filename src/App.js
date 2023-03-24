55dcea5ec7a36c497e5afbc0048e89b7b6e84f28
jdeking14 <jdeking14@gmail.com>
2023-03-24 16:40:36 +0100

    tareas

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ListaDeTareas from './componentes/ListaDeTareas';
+import Logo from './componentes/Logo';
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+      <Logo/>
+      <div className='tareas-lista-principal'>
+        <h1>Mis tareas</h1>
+        <ListaDeTareas/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
